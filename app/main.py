18beb1eb1cd91ac06469e99a6cd71dce4876a05e
ronalcabrera <ronal.cabrera@gmail.com>
2023-01-19 11:42:56 -0300

    test

--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
 from dataframe import amazon_prime, disney_plus, hulu, netflix
+import test
 
 app = FastAPI()
 
--- dockerfile
+++ dockerfile
@@ -1,6 +1,9 @@
 FROM tiangolo/uvicorn-gunicorn-fastapi
 
+RUN pip install pymysql
+RUN pip install sqlalchemy
 RUN pip install pandas
+RUN pip install numpy
 
 EXPOSE 80
 
